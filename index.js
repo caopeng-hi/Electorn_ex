22ccf52d6ab3bd25b4bbbcae5c988064e1e53256
caopeng <caopeng_404@163.com>
2025-04-26 22:36:04 +0800

    完成窗口关闭提示

--- index.js
+++ index.js
@@ -3,7 +3,24 @@ const { getCurrentWindow } = require('@electron/remote') // 正确引入
 
 
 window.addEventListener('DOMContentLoaded', () => {
-    console.log('cp-');
+    window.onbeforeunload = function () {
+        console.log(111);
+        const isClose = document.querySelector('.is-close')
+        if (isClose) {
+            isClose.style.display = 'block'
+            let yes = isClose.getElementById('isClose')
+            let no = isClose.getElementById('noClose')
+            yes.addEventListener('click', () => {
+                // 当前事件需要关闭窗口
+                const mainWin = getCurrentWindow() // 获取当前窗口
+                mainWin.destroy()
+            })
+            no.addEventListener('click', () => {
+                isClose.style.display = 'none'
+            })
+        }
+        return false
+    }
     const mainWin = getCurrentWindow() // 获取当前窗口
     const min = document.getElementById('min')
     const max = document.getElementById('max')
