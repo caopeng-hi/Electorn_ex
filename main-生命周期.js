806132a4bbb0beeffed12014941b28c5338e8d16
caopeng <caopeng_404@163.com>
2025-04-26 17:03:31 +0800

    添加nodemon

--- main-生命周期.js
+++ main-生命周期.js
@@ -16,8 +16,23 @@ app.whenReady().then(() => {
         console.log('close----cp');
         mainWin = null
     })
-})
 
+    mainWin.webContents.on('did-finish-load', () => {
+        console.log('did-finish-load----cp');
+    })
+    mainWin.webContents.on('dom-ready', () => {
+        console.log('dom-ready----cp');
+    })
+})
+app.on('before-quit', () => {
+    console.log('before-quit----cp');
+})
+app.on('will-quit', () => {
+    console.log('will-quit----cp');
+})
+app.on('quit', () => {
+    console.log('quit----cp');
+})
 app.on('window-all-closed', () => {
     console.log('window-all-closed----cp');
     app.quit()
