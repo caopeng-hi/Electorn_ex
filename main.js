8889dc8ae90135412c7dee247f0bc119803c39b2
caopeng <caopeng_404@163.com>
2025-04-26 16:57:15 +0800

    添加生命周期事件监听

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const { app, BrowserWindow } = require('electron')
 
 app.whenReady().then(() => {
+    console.log('ready----cp');
 
     const mainWin = new BrowserWindow({
         width: 600,
@@ -13,12 +14,26 @@ app.whenReady().then(() => {
 
     mainWin.on('close', () => {
         console.log('close----cp');
+        mainWin = null
+    })
 
+    mainWin.webContents.on('did-finish-load', () => {
+        console.log('did-finish-load----cp');
+    })
+    mainWin.webContents.on('dom-ready', () => {
+        console.log('dom-ready----cp');
     })
 })
-
+app.on('before-quit', () => {
+    console.log('before-quit----cp');
+})
+app.on('will-quit', () => {
+    console.log('will-quit----cp');
+})
+app.on('quit', () => {
+    console.log('quit----cp');
+})
 app.on('window-all-closed', () => {
-
-    console.log('close----cp');
+    console.log('window-all-closed----cp');
     app.quit()
 })
